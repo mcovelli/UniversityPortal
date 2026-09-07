3f28ded0e8ff5f2d06ad05731c0975512c6fc463
Mike Covelli <196670706+mcovelli@users.noreply.github.com>
2026-09-07 14:07:36 -0400

    added comment lines to prereq audit

--- prerequisite_audit.sql
+++ prerequisite_audit.sql
@@ -1,3 +1,7 @@
+-- List Courses student enrolled in without taking the required prereq
+
+-- Map Courses with their Prerequisites
+-- Exclude intro courses that do not have prerequisites
 WITH prerequisites AS (
 	SELECT c.CourseID, p.PrerequisiteCourseID, p.MinGradeRequired
 	FROM Course c
@@ -5,6 +9,10 @@ WITH prerequisites AS (
 	GROUP BY c.CourseID, p.PrerequisiteCourseID
 )
 
+-- Select enrolled courses
+-- Join prereqs with enrolled courses
+-- Check if student completed the prerequisite with a passing grade
+-- exclude courses the student already took
 SELECT se.StudentID, se.CourseID, se.Grade, se.Status
 FROM StudentEnrollment se
 JOIN prerequisites p ON p.CourseID = se.CourseID
